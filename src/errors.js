4ae266b607af4068e2d411ce25c1962b0e4dd5ca
Michael Hemesath <mike.hemesath@cerner.com>
2016-05-31 21:06:42 -0500

    Add const keyword to errors

--- src/errors.js
+++ src/errors.js
@@ -1,9 +1,9 @@
 // http://www.jsonrpc.org/specification#error_object
-PARSE_ERROR = Object.freeze({ message: 'Parse error', code: -32700 })
-INVALID_REQUEST = Object.freeze({ message: 'Invalid request', code: -32600 })
-METHOD_NOT_FOUND = Object.freeze({ message: 'Method not found', code: -32601 })
-INVALID_PARAMS = Object.freeze({ message: 'Invalid params', code: -32602 })
-INTERNAL_ERROR = Object.freeze({ message: 'Internal error', code: -32603 })
+const PARSE_ERROR = Object.freeze({ message: 'Parse error', code: -32700 })
+const INVALID_REQUEST = Object.freeze({ message: 'Invalid request', code: -32600 })
+const METHOD_NOT_FOUND = Object.freeze({ message: 'Method not found', code: -32601 })
+const INVALID_PARAMS = Object.freeze({ message: 'Invalid params', code: -32602 })
+const INTERNAL_ERROR = Object.freeze({ message: 'Internal error', code: -32603 })
 
 module.exports = Object.freeze({
   PARSE_ERROR: PARSE_ERROR,
