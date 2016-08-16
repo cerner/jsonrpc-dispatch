b9c616a4d0b91a7fced48b4887acc522af69408c
Michael Hemesath <mike.hemesath@cerner.com>
2016-08-16 10:43:07 -0500

    Update to use Promises. Closes #2 * Update README documentation * Update tests * Remove lib and replace with postinstall script

--- README.md
+++ README.md
@@ -3,11 +3,12 @@
 JSONRPC is a module for managing [JSONRPC](http://json-rpc.org) requests and
 responses in JavaScript.
 
-This implementation is agnostic of the transport mechanism to send and receive JSONRPC
-messages.  This means it can be easily integrated with XMLHttpRequests, HTTPServer and postMessage cross domain messaging. A few of the feature included are:
+This implementation is agnostic of the transport mechanism to send and receive
+JSONRPC messages.  This means it can be easily integrated with XMLHttpRequests,
+HTTPServer and postMessage cross domain messaging.
+A few of the feature included are:
 
-* Async request/response support
-* Callback handling for JSONRPC responses
+* Async request/response support using promises
 * Method handling for JSONRPC requests
 * Method not found handling
 
@@ -26,114 +27,114 @@ import JSONRPC from 'jsonrpc-dispatch'
 ```
 ## Using JSONRPC
 
-Create a JSONRPC instance and pass in an object with function handlers for all supported requests and notifications.
+The JSONRPC takes 2 arguments, a dispatcher and an object mapping of
+methods it will take requests for.
 
-*Requests* are always passed an additional parameter ``next`` which takes 2 arguments (error, result).
 
 ```js
-const jsonrpc = new JSONRPC({
-  // Example request which adds x and y
-  add: function(x,y, next) {
-    next(null, x+y)
-  },
-
-  // Example request which divides x by y
-  divide: function(x,y, next) {
-    if (y==0) {
-      next('cannot divide by zero')
-      return
-    }
-
-    next(null, x/y)
-  },
-
-  // Example notification
-  hello: function() {
-    console.log('a notification occurred')
-  }
-})
+const jsonrpc = new JSONRPC([dispatcher], [methods]);
 ```
 
-### JSONRPC.request
+| name       | type     | description                                                                                                                                 |
+|------------|----------|---------------------------------------------------------------------------------------------------------------------------------------------|
+| dispatcher | function | A handler which is passed the JSONRPC message object to send.                                                                               |
+| methods    | object   | A mapping of method names to function objects which the JSONRPC instance will handle requests for. The functions **must** return a promise. |
+
+### Example
+The below example creates a JSONRPC instance which supports a single API to add
+parmeters together. The result is sent through the dispatcher to the parent
+frame using the `postMessage` API.
+
+```js
+const methods = {
+  add(x,y) {
+    // Always return a promise
+    return Promise.resolve(x + y);
+  }
+};
+
+const dispatcher = (message) => {
+  // Post a message to the parent frame
+  parent.postMessage(message, 'https://trustedDomain.com');
+};
+
+const jsonrpc = new JSONRPC(dispatcher, methods);
+```
+
+### JSONRPC#request
 http://www.jsonrpc.org/specification#request_object
 
-Generates a JSONRPC request object. Request takes 3 parameters
+Sends a JSONRPC request to the provided dispatcher. Request takes 2 parameters
 
-* Method name
-* Method parameters
-* Callback function - The function to execute when the response is received. The function takes an error and a result. If error is present result will be null.  ``this`` references the context passed when handling the incoming response. See JSONRPC.handle for more information.
+| name       | type            | description                                          |
+|------------|-----------------|------------------------------------------------------|
+| name       | String          | The method name to send the request for              |
+| parameters | Array or Object | (Optional) The parameters to pass to the method call |
+
+Returns an instance of a [Promise](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise) which is resolved or rejected when the response is received.
 
 ```js
-const jsonrpc = new JSONRPC({})
-const request = jsonrpc.request('foobar', ['biz', 'baz'], function(error, result) {
-  // TODO: Handle result and error if present
-})
+jsonrpc.request('foobar', ['biz', 'baz']).then((result) => {
+  // TODO: do something with result
+});
 ```
 
-Once the request object is created it can be sent to the JSONRPC service for handling.
 
-
-### JSONRPC.notification
+### JSONRPC#notification
 http://www.jsonrpc.org/specification#notification
 
-Notifications are similar to requests but do not have any responses and thus have no need for a callback. Notifications take 2 parameters:
-
-* Method name
-* Method parameters
-
-```
-const jsonrpc = new JSONRPC({})
-const notification = jsonrpc.request('foobar', ['biz', 'baz'])
-```
-
-Once the notification object is created it can be sent to the JSONRPC service for handling.
+Sends a JSONRPC notification request to the provided dispatcher.
+Notifications are similar to requests but do not have any responses and thus
+do not return a promise. Notifications take 2 parameters:
 
 
-### JSONRPC.handle
-
-Routes incoming messages to be handled as requests, responses or notifications. Handle takes 3 arguments
-
-* message - The JSONRPC message to handle. Can be a request, response or notification
-* context - The context to bind to the response or request handler upon invocation
-* next - The function to execute when the JSONRPC response is ready. Only called for request messages.
+| name       | type            | description                                          |
+|------------|-----------------|------------------------------------------------------|
+| name       | String          | The method name to send the request for              |
+| parameters | Array or Object | (Optional) The parameters to pass to the method call |
 
 ```js
+jsonrpc.notification('foobar', ['biz', 'baz']);
+```
 
-const jsonrpc = new JSONRPC({
-  add: function(x, y, next) {
-    next(null, x + y)
-  }
-})
 
-const request = {
+
+### JSONRPC#handle
+
+Processes incoming JSONRPC messages to be handled as requests, responses
+or notifications. Handle takes a single argument:
+
+| name    | type   | description                                                                                                                                         |
+|---------|--------|-----------------------------------------------------------------------------------------------------------------------------------------------------|
+| message | Object | A JSONRPC [request](http://www.jsonrpc.org/specification#request_object) or [response](http://www.jsonrpc.org/specification#response_object) object |
+
+```js
+jsonrpc.handle({
   id: '1',
   method: 'add',
   params: [1, 2],
   jsonrpc: '2.0'
-}
+});
 
-jsonrpc.handle(request, {}, (response) => {
-  console.log(response)
-})
-
-// { id: '1', jsonrpc: '2.0', result: 3 }
 ```
 
 ## Examples
 
 ### AJAX with jQuery
 
-Here is an example usage using AJAX as a transport mechanism to call a JSONRPC service which handles adding 2 numbers together.
+Here is an example usage using AJAX as a transport mechanism to call a JSONRPC
+service which handles adding 2 numbers together.
 
 ```js
-// 1. Create new JSONRPC instance which only makes requests
-const jsonrpc = new JSONRPC()
-
-// 2. Create a request message
-const request = jsonrpc.request('add', [1,2], function(err, result) {
-  console.log('1 + 2 = ', result)
+// 1. Create new JSONRPC instance which only makes requests.
+// Pass a dispatcher to post the messages to a server using jQuery.
+// Have the dispatcher pass the AJAX response to the jsonrpc#handle method.
+const jsonrpc = new JSONRPC((message) => {
+  $.post('/jsonrpc-service', message, jsonrpc.handle.bind(jsonrpc), 'json')
 })
 
-// 3. Post the JSONRPC message to a server for processing,
-$.post('/jsonrpc-service', request, jsonrpc.handle.bind(jsonrpc), 'json')
+// 2. Send a JSONRPC request then print out the result
+jsonrpc.request('add', [1,2]).then((result) => {
+  console.log('1 + 2 = ', result)
+});
 ```
--- test/test.js
+++ test/test.js
@@ -9,193 +9,171 @@ describe('JSONRPC', () => {
 
   describe('#constructor', () => {
     const methods = { foo() {} };
-    const jsonrpc = new JSONRPC(methods);
+    const dispatcher = () => {};
+    const jsonrpc = new JSONRPC(dispatcher, methods);
 
     it('sets the version', () => expect(jsonrpc.version).to.equal('2.0'));
     it('sets the methods', () => expect(jsonrpc.methods).to.equal(methods));
-    it('sets the callbacks', () => expect(jsonrpc.callbacks).to.be.an('object'));
+    it('sets the deferreds', () => expect(jsonrpc.deferreds).to.be.an('object'));
+    it('sets the dispatcher', () => expect(jsonrpc.dispatcher).to.equal(dispatcher));
   });
 
   describe('#notification', () => {
-    const jsonrpc = new JSONRPC({});
-    const notification = jsonrpc.notification('foo', ['hi']);
-    it('has an id', () => expect(notification.id).to.equal(undefined));
-    it('has a jsonrpc version', () => expect(notification.jsonrpc).to.equal(jsonrpc.version));
-    it('hasa method', () => expect(notification.method).to.equal('foo'));
-    it('has params', () => {
-      expect(notification.params).to.have.lengthOf(1);
-      expect(notification.params[0]).to.equal('hi');
-    });
+    const jsonrpc = new JSONRPC((notification) => {
+      it('has an id', () => expect(notification.id).to.equal(undefined));
+      it('has a jsonrpc version', () => expect(notification.jsonrpc).to.equal(jsonrpc.version));
+      it('has a method', () => expect(notification.method).to.equal('foo'));
+      it('has params', () => {
+        expect(notification.params).to.have.lengthOf(1);
+        expect(notification.params[0]).to.equal('hi');
+      });
+    }, {});
+
+    jsonrpc.notification('foo', ['hi']);
   });
 
   describe('#request', () => {
-    const jsonrpc = new JSONRPC({});
-    const callback = () => {};
-    const request = jsonrpc.request('foo', ['hi'], callback);
+    const jsonrpc = new JSONRPC((request) => {
+      it('has a jsonrpc version', () => expect(request.jsonrpc).to.equal(jsonrpc.version));
+      it('has a method', () => expect(request.method).to.equal('foo'));
+      it('has a unique id', () => {
+        expect(request.id).to.be.a('string');
+        expect(request.id).to.have.lengthOf(36);
+      });
 
-    it('has a jsonrpc version', () => expect(request.jsonrpc).to.equal(jsonrpc.version));
-    it('has a method', () => expect(request.method).to.equal('foo'));
-    it('has a unique id', () => {
-      expect(request.id).to.be.a('string');
-      expect(request.id).to.have.lengthOf(36);
+      it('has params', () => {
+        expect(request.params).to.have.lengthOf(1);
+        expect(request.params[0]).to.equal('hi');
+      });
+
+      it('stores the deferred', () => {
+        const deferred = jsonrpc.deferreds[request.id];
+        expect(deferred).to.be.an('object');
+        expect(deferred.resolve).to.be.a('function');
+        expect(deferred.reject).to.be.a('function');
+      });
     });
 
-    it('has params', () => {
-      expect(request.params).to.have.lengthOf(1);
-      expect(request.params[0]).to.equal('hi');
-    });
-
-    it('stores the callback', () => {
-      expect(jsonrpc.callbacks[request.id]).to.equal(callback);
-    });
+    const promise = jsonrpc.request('foo', ['hi']);
+    it('returns a promise', () => expect(promise).to.be.an('promise'));
   });
 
   describe('#handleRequest', () => {
-    const jsonrpc = new JSONRPC({
-      add(x, y, next) { next(null, x + y); },
-      echoContext(next) { next(null, this); },
-    });
+    const methods = { add(x, y) { return Promise.resolve(x + y); } };
 
     it('responds with JSONRPC response', (done) => {
-      const request = new JSONRPC().request('add', [1, 2]);
-      const context = {};
-      jsonrpc.handleRequest(request, context, (response) => {
-        expect(response.result).to.equal(3);
-        expect(response.jsonrpc).to.equal(request.jsonrpc);
-        expect(response.id).to.equal(request.id);
-        expect(response.error).to.equal(null);
-        done();
-      });
-    });
-
-    it('uses given context to call request handler', (done) => {
-      const request = new JSONRPC().request('echoContext');
-      const context = { test: 'context' };
-      jsonrpc.handleRequest(request, context, (response) => {
-        expect(response.result).to.equal(context);
-        done();
-      });
-    });
-
-    it('handles unknown requests with a method not found error', (done) => {
-      const request = new JSONRPC().request('doesntExist', () => {});
-      jsonrpc.handleRequest(request, {}, (response) => {
-        expect(response.result).to.equal(undefined);
-        expect(response.id).to.equal(request.id);
-        expect(response.error).to.equal(ERRORS.METHOD_NOT_FOUND);
-        done();
-      });
-    });
-  });
-
-  describe('#handleNotification', () => {
-    it('executes the notification handler', (done) => {
-      const request = new JSONRPC().notification('foo', ['bar', 'baz']);
-      const context = {};
-      const jsonrpc = new JSONRPC({
-        foo(x, y, next) {
-          expect(x).to.equal('bar');
-          expect(y).to.equal('baz');
-          expect(this).to.equal(context);
-          expect(next).to.equal(undefined);
-          done();
-        },
-      });
-
-      jsonrpc.handleNotification(request, context);
-    });
-
-    it('handles unknown notifications silently', () => {
-      const request = new JSONRPC().notification('foo');
-      const jsonrpc = new JSONRPC({});
-      jsonrpc.handleNotification(request);
-    });
-  });
-
-  describe('#handleResponse', () => {
-    const jsonrpc = new JSONRPC({});
-
-    it('executes the response handler', (done) => {
-      const context = { bar: 'baz' };
-      const request = jsonrpc.request('foo', [], function f(error, result) {
-        expect(this).to.equal(context);
-        expect(error).to.equal(undefined);
-        expect(result).to.equal('foo');
-        done();
-      });
-
-      jsonrpc.handleResponse({ id: request.id, result: 'foo' }, context);
-    });
-
-    it('passes errors to the response handler', (done) => {
-      const context = { bar: 'baz' };
-      const request = jsonrpc.request('foo', [], function f(error, result) {
-        expect(this).to.equal(context);
-        expect(error).to.equal(ERRORS.METHOD_NOT_FOUND);
-        expect(result).to.equal(undefined);
-        done();
-      });
-
-      jsonrpc.handleResponse({ id: request.id, error: ERRORS.METHOD_NOT_FOUND }, context);
-    });
-
-    it('handles unknown responses silently', () => {
-      jsonrpc.handleResponse({ id: 'abc123', result: 'foo' }, {});
-    });
-  });
-
-  describe('#handle', () => {
-    it('handles notifications', (done) => {
-      const request = new JSONRPC().notification('foo', ['bar', 'baz']);
-      const context = {};
-      const jsonrpc = new JSONRPC({
-        foo(x, y, next) {
-          expect(x).to.equal('bar');
-          expect(y).to.equal('baz');
-          expect(this).to.equal(context);
-          expect(next).to.equal(undefined);
-          done();
-        },
-      });
-
-      jsonrpc.handle(request, context);
-    });
-
-    it('handles requests', (done) => {
-      const request = new JSONRPC().request('add', [1, 2]);
-      const context = {};
-      const jsonrpc = new JSONRPC({
-        add(x, y, next) {
-          expect(x).to.equal(1);
-          expect(y).to.equal(2);
-          expect(this).to.equal(context);
-          expect(next).to.be.a('function');
-          next(undefined, x + y);
-        },
-      });
-
-      function callback(response) {
+      const request = { id: 1111, method: 'add', params: [1, 2], jsonrpc: '2.0' };
+      const jsonrpc = new JSONRPC((response) => {
         expect(response.result).to.equal(3);
         expect(response.jsonrpc).to.equal(request.jsonrpc);
         expect(response.id).to.equal(request.id);
         expect(response.error).to.equal(undefined);
         done();
-      }
+      }, methods);
 
-      jsonrpc.handle(request, context, callback);
+      jsonrpc.handleRequest(request);
     });
 
-    it('handles response', (done) => {
-      const jsonrpc = new JSONRPC({});
-      const context = { bar: 'baz' };
-      const request = jsonrpc.request('foo', [], function callback(error, result) {
-        expect(this).to.equal(context);
-        expect(error).to.equal(undefined);
+
+    it('handles unknown requests with a method not found error', (done) => {
+      const request = { id: 2222, method: 'unknown', params: [1, 2], jsonrpc: '2.0' };
+      const jsonrpc = new JSONRPC((response) => {
+        expect(response.result).to.equal(undefined);
+        expect(response.id).to.equal(request.id);
+        expect(response.error.code).to.equal(ERRORS.METHOD_NOT_FOUND);
+        done();
+      }, methods);
+
+      jsonrpc.handleRequest(request);
+    });
+  });
+
+  describe('#handleNotification', () => {
+    it('executes the notification handler', () => {
+      const request = { method: 'foo', params: ['bar', 'baz'] };
+      const jsonrpc = new JSONRPC(() => {}, {
+        foo(x, y) {
+          expect(x).to.equal(request.params[0]);
+          expect(y).to.equal(request.params[1]);
+        },
+      });
+
+      jsonrpc.handleNotification(request);
+    });
+
+    it('handles unknown notifications silently', (done) => {
+      const jsonrpc = new JSONRPC(() => {});
+      jsonrpc.handleNotification({ method: 'foo', params: ['bar', 'baz'] });
+      done();
+    });
+  });
+
+  describe('#handleResponse', () => {
+    it('executes the response handler', (done) => {
+      const jsonrpc = new JSONRPC((request) => {
+        jsonrpc.handleResponse({ id: request.id, result: 'foo' });
+      });
+
+      jsonrpc.request('foo').then((result) => {
         expect(result).to.equal('foo');
         done();
       });
+    });
 
-      jsonrpc.handle({ id: request.id, result: 'foo' }, context);
+    it('passes errors to the response handler', (done) => {
+      const error = { code: ERRORS.METHOD_NOT_FOUND };
+      const jsonrpc = new JSONRPC((request) => {
+        jsonrpc.handleResponse({ id: request.id, error });
+      });
+
+      jsonrpc.request('foo').catch((message) => {
+        expect(message).to.equal(error);
+        done();
+      });
+    });
+
+    it('handles unknown responses silently', () => {
+      const jsonrpc = new JSONRPC(() => {});
+      jsonrpc.handleResponse({ id: 'abc123', result: 'foo' });
+    });
+  });
+
+  describe('#handle', () => {
+    it('handles notifications', () => {
+      const request = { method: 'foo', params: ['bar', 'baz'] };
+      const jsonrpc = new JSONRPC(() => {}, {
+        foo(x, y) {
+          expect(x).to.equal(request.params[0]);
+          expect(y).to.equal(request.params[1]);
+        },
+      });
+
+      jsonrpc.handle(request);
+    });
+
+    it('handles requests', (done) => {
+      const methods = { add(x, y) { return Promise.resolve(x + y); } };
+      const request = { id: 1111, method: 'add', params: [1, 2], jsonrpc: '2.0' };
+      const jsonrpc = new JSONRPC((response) => {
+        expect(response.result).to.equal(3);
+        expect(response.jsonrpc).to.equal(request.jsonrpc);
+        expect(response.id).to.equal(request.id);
+        expect(response.error).to.equal(undefined);
+        done();
+      }, methods);
+
+      jsonrpc.handleRequest(request);
+    });
+
+    it('handles response', (done) => {
+      const jsonrpc = new JSONRPC((request) => {
+        jsonrpc.handleResponse({ id: request.id, result: 'foo' });
+      });
+
+      jsonrpc.request('foo').then((result) => {
+        expect(result).to.equal('foo');
+        done();
+      });
     });
   });
 });
